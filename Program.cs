fcb11a18afb12d2baf0c850563ec912730c4df0a
Erik Berns <erik.berns@gmx.de>
2024-01-07 18:05:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Tell me a number to count to");
+string numberText = Console.ReadLine();
+int number = Convert.ToInt32(numberText);
+
+Console.Clear();
+
+
+Count(number);
+
+
+
+void Count(int numberToCountTo)
+{   
+    for (int i = 1; i <= numberToCountTo; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
